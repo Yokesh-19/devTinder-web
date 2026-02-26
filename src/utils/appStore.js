e0032409a0f732f36d8ee5f7d24b2d368427a0c9
Yokesh M <727722eucs208@skcet.ac.in>
2026-02-26 19:22:02 +0530

    feature - Connection Page

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./useSlice";
 import feedReducer from "./feedSlice";
+import connectionReducer from "./connectionSlice";
+
+
 
 const appStore = configureStore({
     reducer:{
         user: userReducer,
         feed: feedReducer,
+        connections: connectionReducer,
     },
 });
 
